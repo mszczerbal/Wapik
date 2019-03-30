b7c2db11d9e9625c4333420b2c3be31b5fc78121
Mariusz <szczerbal.mariusz@gmail.com>
2019-03-30 10:39:36 +0100

    Finishing with header

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const navbarToggle = document.querySelector('.navbar-toggle');
 const menu = document.querySelector('nav ul');
+const headerInfo = document.querySelector('.header-info');
 
 navbarToggle.addEventListener('click', function() {
     menu.classList.toggle('active');
+    headerInfo.classList.toggle('active');
 });
